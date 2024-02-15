d9d471ae32b43f867e36ee1f3936a745cd938e58
santiago <scarnezis@gmail.com>
2024-02-15 18:05:21 -0300

    jenkinsfile 29

--- src/test/java/com/scarnezis/ticketing/TicketingApplicationTests.java
+++ src/test/java/com/scarnezis/ticketing/TicketingApplicationTests.java
@@ -2,9 +2,18 @@ package com.scarnezis.ticketing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+//@AutoConfigureTestDatabase
+//@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 class TicketingApplicationTests {
 
 	@Test
